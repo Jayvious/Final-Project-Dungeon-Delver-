520fe2d0ae4c84ebc5b25eee18d9dc29a8122246
Jayvious <54966469+Jayvious@users.noreply.github.com>
2019-11-21 21:31:33 -0500

    woah we actually doing things
    Creation of the Tile script and adding some code

---
+++ Assets/__Scripts/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public int x;
+    public int y;
+    public int tileNum;
+    
+    public void SetTile(int eX, int eY, int eTileName = -1)
+    {
+        x = eX;
+        y = eY;
+        transform.localPosition = new Vector3(x, y, 0);
+        gameObject.name = x.ToString("D3") + "X" + y.ToString("D3");
+
+        if(eTileName == -1)
+        {
+            eTileName = TileCamera.GET_MAP(x, y);
+        }
+
+        tileNum = eTileName;
+        GetComponent<SpriteRenderer>().sprite = TileCamera.SPRITES(tileNum);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
