922cd943b4c4ea366ab16add52707ab8f08c6605
Jayvious <54966469+Jayvious@users.noreply.github.com>
2019-11-21 21:59:00 -0500

    TileCamera script

---
+++ Assets/__Scripts/TileCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCamera : MonoBehaviour
+{
+    static private int W, H;
+    static private int[,] MAP;
+    static public Sprite[] SPRITES;
+    static public Transform TILE_ANCHOR;
+    static public Tile[,] TILES;
+
+    [Header("Set in Inscpector")]
+    public TextAsset mapData;
+    public Texture2D mapTiles;
+    public TextAsset mapCollisions; //this will be used later
+    public Tile tilePrefab;
+
+     void Awake()
+    {
+        LoadMap();  
+    }
+
+    public void LoadMap()
+    {
+        GameObject go = new GameObject("TILE_ANCHOR");
+        TILE_ANCHOR = go.transform;
+
+        SPRITES = Resources.LoadAll<Sprite>(mapTiles.name);
+
+        string[] lines = mapData.text.Split('\n');
+        H = lines.Length;
+        string[] tileNums = lines[0].Split(' ');
+        W = tileNums.Length;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
