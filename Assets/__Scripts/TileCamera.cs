d041b8d7ed0cb639ed81270bc97ff3cb6c03b3e2
Jayvious <JayviousWilliams@gmail.com>
2019-11-26 19:39:56 -0500

    changes to script
    Tile.cs
    TileCamera.cs

--- Assets/__Scripts/TileCamera.cs
+++ Assets/__Scripts/TileCamera.cs
@@ -43,10 +43,66 @@ public class TileCamera : MonoBehaviour
             tileNums = lines[j].Split(' ');
             for(int i = 0; i<W; i++)
             {
+                if(tileNums[i] == "..")
+                {
+                    MAP[i, j] = 0;
+                }
+                else
+                {
+                    MAP[i, j] = int.Parse(tileNums[i], hexNum);
+                }
+            }
+        }
+        print("Parsed " + SPRITES.Length + " sprites.");
+        print("Map size: " + W + " wide by " + H + " high");
 
+        ShowMap();
+    }
+    void ShowMap()
+    {
+        TILES = new Tile[W, H];
+
+        for(int j = 0; j<H; j++)
+        {
+            for(int i = 0; i<W; i++)
+            {
+                if(MAP[i, j] != 0)
+                {
+                    Tile ti = Instantiate<Tile>(tilePrefab);
+                    ti.transform.SetParent(TILE_ANCHOR);
+                    ti.SetTile(i, j);
+                    TILES[i, j] = ti;
+                }
             }
         }
     }
+    static public int GET_MAP(int x, int y)
+    {
+        if(x<0 || x>= W || y<0 || y>= H)
+        {
+            return -1;
+        }
+        return MAP[x, y];
+
+    }
+
+    static public int GET_MAP(float x, float y)
+    {
+        int tX = Mathf.RoundToInt(x);
+        int tY = Mathf.RoundToInt(y);
+
+        return GET_MAP(tX, tY);
+    }
+
+    static public void SET_MAP(int x, int y, int tNum)
+    {
+        if(x<0 || x>=W || y<0 || y >= H)
+        {
+            return;
+        }
+        MAP[x, y] = tNum;
+    }
+
 
     // Start is called before the first frame update
     void Start()
