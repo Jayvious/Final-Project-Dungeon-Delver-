b5af1f32675ae2cf6af261bf8e070a99d879ae55
Jayvious <JayviousWilliams@gmail.com>
2019-11-26 21:58:12 -0500

    Animation and script all in one script
    Wow

--- Assets/__Scripts/Dray.cs
+++ Assets/__Scripts/Dray.cs
@@ -11,14 +11,18 @@ public class Dray : MonoBehaviour
     public int dirHeld = -1;
 
     private Rigidbody rigid;
+    private Animator anim;
 
     private Vector3[] directions = new Vector3[]
     { Vector3.right, Vector3.up, Vector3.left, Vector3.down};
 
+    private KeyCode[] keys = new KeyCode[]
+    {KeyCode.RightArrow, KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow };
 
      void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
          
     }
 
@@ -34,14 +38,25 @@ public class Dray : MonoBehaviour
     void Update()
     {
         dirHeld = -1;
-        if (Input.GetKey(KeyCode.RightArrow)) dirHeld = 0;
-        if (Input.GetKey(KeyCode.UpArrow)) dirHeld = 1;
-        if (Input.GetKey(KeyCode.LeftArrow)) dirHeld = 2;
-        if (Input.GetKey(KeyCode.DownArrow)) dirHeld = 3;
+       
+        for(int i = 0; i<4; i++)
+        {
+            if (Input.GetKey(keys[i])) dirHeld = i;
+        }
 
         Vector3 vel = Vector3.zero;
         if (dirHeld > -1) vel = directions[dirHeld];
 
         rigid.velocity = vel * speed;
+
+        if(dirHeld == -1)
+        {
+            anim.speed = 0;
+        }
+        else
+        {
+            anim.CrossFade("Dray_Walk_" + dirHeld, 0);
+            anim.speed = 1;
+        }
     }
 }
